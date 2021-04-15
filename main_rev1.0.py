da090c8f5e58b51011962538c41ad7c151dbb353
Manolo <manolo.b@outlook.com>
2021-04-16 01:01:07 +0200

    dictionary added, added command 'facts'

--- main_rev1.0.py
+++ main_rev1.0.py
@@ -1,4 +1,6 @@
 import discord
+import random
+from shrek_dictionary import *
 from decouple import config
 from discord.ext import commands
 
@@ -35,7 +37,7 @@ def guildMembers(ml, lp):  # FUNCTION TO LIST ALL GUILD MEMBERS
         if lp == "name":
             ml = f'\n - '.join([member.name for member in guild.members])
         elif lp == "id":
-            ml = f'\n - '.join([member.id for member in guild.members])
+            ml = f'\n - '.join([str(member.id) for member in guild.members])
     return ml
 
 
@@ -53,18 +55,11 @@ async def swamp(ctx):
     await ctx.send(f"GET OUT OF ME SWAMP YA OLD CHUM'")
 
 @client.command(aliases=['ml'])
-
-async def ml_parameter_missing(ctx):
-    parameter = ['name','id']
-    await ctx.reply(invalidParameter(parameter))
-
 async def memberList(ctx, lp):
+    parameter = ['name','id']
 
     if lp != "name" and lp != "id":
-        await ctx.send(
-            f'Parameter "{lp}" is unvalid!"'
-            f'\nPlease use either "name" or "id" as parameter.'
-            )
+        await ctx.reply(invalidParameter(parameter))
     
     else:
         await ctx.send(
@@ -72,9 +67,42 @@ async def memberList(ctx, lp):
             f'\n\n - {guildMembers("member list", lp)}'
             )
 
-@client.command(aliases=['sw@'])
-async def swampAt(ctx):
-    pass
+@client.command(aliases=['@'])
+async def swampAt(ctx, name):
+    memberNames = []
+    for member in guild.members:
+        memberNames.append(member.name)
+        if name == member.name:
+            await ctx.send(f"{member.mention} YOU BETTER GET OUT OF ME SWAMP BEFORE I SLAP YOUR STUPID ASS")
+            break
+    if name not in memberNames:
+        await ctx.send(f'{name} is not a valid username!')
 
+
+@client.command(aliases=['fa'])
+async def swampFact(ctx):
+
+    channel = ctx.channel
+    random_fact = random.choice(fact_list)
+    fact_index = fact_list.index(random_fact)
+    await ctx.send(random_fact)
+
+    def check(ans):
+        if ans.content == answer_list[fact_index] and ans.channel == channel:
+            return ans.content, ans.channel
+    if  answer_list[fact_index] == 'null':
+        print('break; no answer expected')
+    else:
+        answer = await client.wait_for('message', check=check, timeout=15)
+        await answer.reply(f'Success!') 
+
+    '''
+    @client.event 
+    async def on_message(message):
+        if message.content != "null" and message.content == answer_list[i]:
+            await ctx.reply(f'Bon toutou')
+        else:
+            await ctx.reply(f'test')
+    '''
 
 client.run(TOKEN)
