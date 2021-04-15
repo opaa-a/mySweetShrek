174def4e653e328cfaaa8872a3d39a27845870d2
Manolo <manolo.b@outlook.com>
2021-04-16 01:49:40 +0200

    Added default channel, added reload/load message to default chan.

--- main_rev1.0.py
+++ main_rev1.0.py
@@ -6,12 +6,16 @@ from discord.ext import commands
 
 TOKEN = config('DISCORD_TOKEN') # BOT TOKEN
 GUILD = config('DISCORD_GUILD') # DISCORD SERVER TOKEN
+TEST_CHAN = config('DISCORD_TEST_CHAN') # DISCORD DEFAULT/ TEST CHANNEL
 
 intents = discord.Intents(messages=True,guilds=True,reactions=True,members=True,presences=True)   # SETUP INTENTS
 client = commands.Bot(command_prefix= '!', intents=intents)  # SETUP THE CLIENT & PREFIX OF COMMANDS
 
 @client.event
 async def on_ready():
+
+    test_chan = client.get_channel(int(TEST_CHAN))
+
     global guild
     for guild in client.guilds: # CHECK FOR THE GUILD BOT IS IN.
         if guild.name == GUILD:
@@ -24,37 +28,39 @@ async def on_ready():
         f'\n - Guild ID: {guild.id}'
     )
 
+    await test_chan.send(
+        f'{client.user} Is lit and drip :joy:'
+        )
+
 def unfoldList(listName):
     unfold = f'\n - '.join([i for i in listName])
     return unfold
     
 
 def guildMembers(ml, lp):  # FUNCTION TO LIST ALL GUILD MEMBERS
-   
     ml = []
-
-    for member in guild.members:
+    for member in guild.members:        # LP = LIST PARAMETER / ML = MEMBER LIST
         if lp == "name":
             ml = f'\n - '.join([member.name for member in guild.members])
         elif lp == "id":
             ml = f'\n - '.join([str(member.id) for member in guild.members])
-    return ml
+    return ml   
 
 
-def invalidParameter(parameters):
-    error_msg = f'Invalid Parameter, try again using:\n - {unfoldList(parameters)}'
-    return error_msg
+def invalidParameter(parameters):       # DEFAULT MESSAGE WHEN NO PARAMETERS ARE GIVEN
+    error_parameter = f'Invalid Parameter, try again using:\n - {unfoldList(parameters)}'
+    return error_parameter
         
 
-@client.command(aliases=['p'])      # COMMAND PING
+@client.command(aliases=['p'])      # PING COMMAND
 async def ping(ctx):
     await ctx.send(f'{round(client.latency * 1000)} ms')
 
-@client.command(aliases=['sw'])
+@client.command(aliases=['sw'])     # SWAMP COMMAND
 async def swamp(ctx):
     await ctx.send(f"GET OUT OF ME SWAMP YA OLD CHUM'")
 
-@client.command(aliases=['ml'])
+@client.command(aliases=['ml'])    # MEMBER LIST COMMAND -- NAME OR ID AS PARAMETER
 async def memberList(ctx, lp):
     parameter = ['name','id']
 
@@ -67,7 +73,7 @@ async def memberList(ctx, lp):
             f'\n\n - {guildMembers("member list", lp)}'
             )
 
-@client.command(aliases=['@'])
+@client.command(aliases=['@'])      # SWAMP AT COMMAND --  USERNAME AS PARAMETER
 async def swampAt(ctx, name):
     memberNames = []
     for member in guild.members:
@@ -94,15 +100,6 @@ async def swampFact(ctx):
         print('break; no answer expected')
     else:
         answer = await client.wait_for('message', check=check, timeout=15)
-        await answer.reply(f'Success!') 
-
-    '''
-    @client.event 
-    async def on_message(message):
-        if message.content != "null" and message.content == answer_list[i]:
-            await ctx.reply(f'Bon toutou')
-        else:
-            await ctx.reply(f'test')
-    '''
+        await answer.reply(f'Success! :joy::ok_hand:') 
 
 client.run(TOKEN)
