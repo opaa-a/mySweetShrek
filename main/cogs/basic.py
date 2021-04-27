3fbd55d9d2d7fdffca9cd3e019bca0fafe9c7131
Manolo <manolo.b@outlook.com>
2021-04-28 00:39:36 +0200

    cleaned code of basic.py

--- main/cogs/basic.py
+++ main/cogs/basic.py
@@ -1,17 +1,25 @@
 import discord
 from discord.ext import commands
 
-class Basic_Commands(commands.Cog):
 
+#---------------------------------------------------------------------------------------#      BASIC COMMANDS      #---------------------------------------------------------------------------------------#
+
+# Basic Commands regroup all the the useless/basic commands of the bot.
+class Basic_Commands(commands.Cog):
     def __init__(self, client):
         self.client = client
         print(f"\n- Basic Commands from basic is loaded.")
-    
-                # !swamp@ OR !swampAt -- MENTION A SPECIFIED MEMBER AND SEND TEXT
-    @commands.command(aliases=['swamp@'])
-    async def swampAt(self, ctx, member : discord.Member):
-        await ctx.send(f"OI {member.mention}, GET THE FUCK OUT OF ME SWAMP YA FUCKING TWAT!")
 
+
+# !swamp@ OR !swampAt -- Take one arg, userID. Send the userID a spam message.
+    @commands.command(aliases=['swamp@'])
+    async def swampAt(self, ctx, userID : discord.Member):
+        await ctx.send(f"OI {userID.mention}, GET THE FUCK OUT OF ME SWAMP YA FUCKING TWAT!")
+
+
+#---------------------------------------------------------------------------------------#       BASIC ERROR        #---------------------------------------------------------------------------------------#
+
+# !swamp@ error display
     @swampAt.error
     async def error(self, ctx, error):
 
@@ -23,6 +31,7 @@ class Basic_Commands(commands.Cog):
             return
 
 
+#---------------------------------------------------------------------------------------#       COGS SETUP       #---------------------------------------------------------------------------------------#
 
 def setup(client):
     client.add_cog(Basic_Commands(client))
